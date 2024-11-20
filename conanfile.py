927dbfa5ca234cbdda9bc25744abb49d5edf1a67
Pieter Pas <pieter.p.dev@outlook.com>
2024-11-20 13:06:02 +0100

    Bump conanfile version

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.build import can_run
 
 class GuanaqoRecipe(ConanFile):
     name = "guanaqo"
-    version = "1.0.0-alpha.1"
+    version = "1.0.0-alpha.2"
 
     # Optional metadata
     license = "LGPLv3"
