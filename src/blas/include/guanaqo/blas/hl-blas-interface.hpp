f0c4773cd1b508966a060dac1f6be102ccb5c4ef
Pieter Pas <pieter.p.dev@outlook.com>
2025-06-10 13:13:48 +0200

    Row-major gemm in hl-blas-interface

--- src/blas/include/guanaqo/blas/hl-blas-interface.hpp
+++ src/blas/include/guanaqo/blas/hl-blas-interface.hpp
@@ -20,6 +20,9 @@
 
 namespace guanaqo::blas {
 
+template <class I>
+using UnitStride = std::integral_constant<I, 1>;
+
 template <class T, class I>
 void xgemv_N(T alpha, std::type_identity_t<MatrixView<const T, I>> A,
              std::type_identity_t<MatrixView<const T, I>> x, T beta,
@@ -47,55 +50,89 @@ void xgemv_T(T alpha, std::type_identity_t<MatrixView<const T, I>> A,
 }
 
 template <class T, class I>
-void xgemm_NN(T alpha, std::type_identity_t<MatrixView<const T, I>> A,
-              std::type_identity_t<MatrixView<const T, I>> B, T beta,
-              MatrixView<T, I> C) {
+void xgemv_N(T alpha, std::type_identity_t<MatrixViewRM<const T, I>> A,
+             std::type_identity_t<MatrixView<const T, I>> x, T beta,
+             MatrixView<T, I> y) {
+    GUANAQO_ASSUME(A.rows == y.rows);
+    GUANAQO_ASSUME(A.cols == x.rows);
+    GUANAQO_ASSUME(x.cols == 1);
+    GUANAQO_ASSUME(y.cols == 1);
+    GUANAQO_TRACE_HL_BLAS("gemv", 0, A.rows * A.cols);
+    xgemv<T, I>(CblasRowMajor, CblasNoTrans, A.rows, A.cols, alpha, A.data,
+                A.outer_stride, x.data, I{1}, beta, y.data, I{1});
+}
+
+template <class T, class I>
+void xgemv_T(T alpha, std::type_identity_t<MatrixViewRM<const T, I>> A,
+             std::type_identity_t<MatrixView<const T, I>> x, T beta,
+             MatrixView<T, I> y) {
+    GUANAQO_ASSUME(A.cols == y.rows);
+    GUANAQO_ASSUME(A.rows == x.rows);
+    GUANAQO_ASSUME(x.cols == 1);
+    GUANAQO_ASSUME(y.cols == 1);
+    GUANAQO_TRACE_HL_BLAS("gemv", 0, A.rows * A.cols);
+    xgemv<T, I>(CblasRowMajor, CblasTrans, A.rows, A.cols, alpha, A.data,
+                A.outer_stride, x.data, I{1}, beta, y.data, I{1});
+}
+
+template <class T, class I, StorageOrder O>
+void xgemm_NN(T alpha,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> A,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> B,
+              T beta, MatrixView<T, I, UnitStride<I>, O> C) {
     GUANAQO_ASSUME(A.rows == C.rows);
     GUANAQO_ASSUME(A.cols == B.rows);
     GUANAQO_ASSUME(B.cols == C.cols);
     GUANAQO_TRACE_HL_BLAS("gemm", 0, C.rows * C.cols * A.cols);
-    xgemm<T, I>(CblasColMajor, CblasNoTrans, CblasNoTrans, C.rows, C.cols,
-                A.cols, alpha, A.data, A.outer_stride, B.data, B.outer_stride,
-                beta, C.data, C.outer_stride);
+    xgemm<T, I>(O == StorageOrder::RowMajor ? CblasRowMajor : CblasColMajor,
+                CblasNoTrans, CblasNoTrans, C.rows, C.cols, A.cols, alpha,
+                A.data, A.outer_stride, B.data, B.outer_stride, beta, C.data,
+                C.outer_stride);
 }
 
-template <class T, class I>
-void xgemm_TN(T alpha, std::type_identity_t<MatrixView<const T, I>> A,
-              std::type_identity_t<MatrixView<const T, I>> B, T beta,
-              MatrixView<T, I> C) {
+template <class T, class I, StorageOrder O>
+void xgemm_TN(T alpha,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> A,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> B,
+              T beta, MatrixView<T, I, UnitStride<I>, O> C) {
     GUANAQO_ASSUME(A.cols == C.rows);
     GUANAQO_ASSUME(A.rows == B.rows);
     GUANAQO_ASSUME(B.cols == C.cols);
     GUANAQO_TRACE_HL_BLAS("gemm", 0, C.rows * C.cols * A.rows);
-    xgemm<T, I>(CblasColMajor, CblasTrans, CblasNoTrans, C.rows, C.cols, A.rows,
-                alpha, A.data, A.outer_stride, B.data, B.outer_stride, beta,
-                C.data, C.outer_stride);
+    xgemm<T, I>(O == StorageOrder::RowMajor ? CblasRowMajor : CblasColMajor,
+                CblasTrans, CblasNoTrans, C.rows, C.cols, A.rows, alpha, A.data,
+                A.outer_stride, B.data, B.outer_stride, beta, C.data,
+                C.outer_stride);
 }
 
-template <class T, class I>
-void xgemm_TT(T alpha, std::type_identity_t<MatrixView<const T, I>> A,
-              std::type_identity_t<MatrixView<const T, I>> B, T beta,
-              MatrixView<T, I> C) {
+template <class T, class I, StorageOrder O>
+void xgemm_TT(T alpha,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> A,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> B,
+              T beta, MatrixView<T, I, UnitStride<I>, O> C) {
     GUANAQO_ASSUME(A.cols == C.rows);
     GUANAQO_ASSUME(A.rows == B.cols);
     GUANAQO_ASSUME(B.rows == C.cols);
     GUANAQO_TRACE_HL_BLAS("gemm", 0, C.rows * C.cols * A.rows);
-    xgemm<T, I>(CblasColMajor, CblasTrans, CblasTrans, C.rows, C.cols, A.rows,
-                alpha, A.data, A.outer_stride, B.data, B.outer_stride, beta,
-                C.data, C.outer_stride);
+    xgemm<T, I>(O == StorageOrder::RowMajor ? CblasRowMajor : CblasColMajor,
+                CblasTrans, CblasTrans, C.rows, C.cols, A.rows, alpha, A.data,
+                A.outer_stride, B.data, B.outer_stride, beta, C.data,
+                C.outer_stride);
 }
 
-template <class T, class I>
-void xgemm_NT(T alpha, std::type_identity_t<MatrixView<const T, I>> A,
-              std::type_identity_t<MatrixView<const T, I>> B, T beta,
-              MatrixView<T, I> C) {
+template <class T, class I, StorageOrder O>
+void xgemm_NT(T alpha,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> A,
+              std::type_identity_t<MatrixView<const T, I, UnitStride<I>, O>> B,
+              T beta, MatrixView<T, I, UnitStride<I>, O> C) {
     GUANAQO_ASSUME(A.rows == C.rows);
     GUANAQO_ASSUME(A.cols == B.cols);
     GUANAQO_ASSUME(B.rows == C.cols);
     GUANAQO_TRACE_HL_BLAS("gemm", 0, C.rows * C.cols * A.cols);
-    xgemm<T, I>(CblasColMajor, CblasNoTrans, CblasTrans, C.rows, C.cols, A.cols,
-                alpha, A.data, A.outer_stride, B.data, B.outer_stride, beta,
-                C.data, C.outer_stride);
+    xgemm<T, I>(O == StorageOrder::RowMajor ? CblasRowMajor : CblasColMajor,
+                CblasNoTrans, CblasTrans, C.rows, C.cols, A.cols, alpha, A.data,
+                A.outer_stride, B.data, B.outer_stride, beta, C.data,
+                C.outer_stride);
 }
 
 template <class T, class I>
