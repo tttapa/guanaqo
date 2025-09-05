ad58fa0be92f6ed2551d03f11b71c860e5d09f31
Pieter Pas <pieter.p.dev@outlook.com>
2025-09-05 12:32:20 +0200

    [Test] Improve Eigen matchers

--- test/test-util/eigen-matchers.hpp
+++ test/test-util/eigen-matchers.hpp
@@ -3,10 +3,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <guanaqo/demangled-typename.hpp>
 #include <guanaqo/eigen/view.hpp>
 #include <guanaqo/print.hpp>
 
 #include <Eigen/Core>
+#include <limits>
 
 /// @file
 /// @see https://google.github.io/googletest/reference/matchers.html#defining-matchers
@@ -20,8 +22,30 @@ void print(std::ostream &os, const T &arg) {
 }
 } // namespace guanaqo_test
 
+// Suppress googletest's own output:
+namespace Eigen {
+inline void PrintTo(const Eigen::MatrixXd &mat, std::ostream *os) {
+    *os << "Eigen::MatrixXd of shape (" << mat.rows() << "x" << mat.cols()
+        << ") (output suppressed)";
+}
+inline void PrintTo(const Eigen::MatrixXf &mat, std::ostream *os) {
+    *os << "Eigen::MatrixXf of shape (" << mat.rows() << "x" << mat.cols()
+        << ") (output suppressed)";
+}
+template <typename Derived>
+void PrintTo(const Derived &mat, std::ostream *os)
+    requires std::is_base_of_v<DenseBase<Derived>, Derived>
+{
+    *os << "Eigen expression (" << guanaqo::demangled_typename(typeid(Derived))
+        << ") of shape (" << mat.rows() << "x" << mat.cols()
+        << ") (output suppressed)";
+}
+} // namespace Eigen
+
 MATCHER_P(EigenEqual, expect, "") {
     auto diffnorm = (arg - expect).template lpNorm<Eigen::Infinity>();
+    if (!arg.allFinite())
+        diffnorm = std::numeric_limits<decltype(diffnorm)>::quiet_NaN();
     if (auto *os = result_listener->stream()) {
         *os << "\nactual = ...\n";
         guanaqo_test::print(*os, arg);
@@ -36,6 +60,8 @@ MATCHER_P(EigenEqual, expect, "") {
 
 MATCHER_P2(EigenAlmostEqual, expect, atol, "") {
     auto diffnorm = (arg - expect).template lpNorm<Eigen::Infinity>();
+    if (!arg.allFinite())
+        diffnorm = std::numeric_limits<decltype(diffnorm)>::quiet_NaN();
     if (auto *os = result_listener->stream()) {
         *os << "\nactual = ...\n";
         guanaqo_test::print(*os, arg);
@@ -50,8 +76,12 @@ MATCHER_P2(EigenAlmostEqual, expect, atol, "") {
 }
 
 MATCHER_P2(EigenAlmostEqualRel, expect, rtol, "") {
+    auto denom =
+        expect.unaryExpr([](auto x) { return x == 0 ? decltype(x)(1) : x; });
     auto diffnorm =
-        (arg - expect).cwiseQuotient(expect).template lpNorm<Eigen::Infinity>();
+        (arg - expect).cwiseQuotient(denom).template lpNorm<Eigen::Infinity>();
+    if (!arg.allFinite())
+        diffnorm = std::numeric_limits<decltype(diffnorm)>::quiet_NaN();
     if (auto *os = result_listener->stream()) {
         *os << "\nactual = ...\n";
         guanaqo_test::print(*os, arg);
