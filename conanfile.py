88f6134f8afc5d7eee79d80af3a94705711fce6d
Pieter Pas <pieter.p.dev@outlook.com>
2026-02-14 16:13:21 +0100

    Version 1.0.0-alpha.23

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ from conan.tools.scm import Git
 
 class GuanaqoRecipe(ConanFile):
     name = "guanaqo"
-    version = "1.0.0-alpha.22"
+    version = "1.0.0-alpha.23"
 
     # Optional metadata
     license = "LGPL-3.0-or-later"
